25848d534a4e5a419b362ff639859f4347c951dc
Thomas Nguyen <ttn@ou.edu>
2022-08-31 21:06:14 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { UserProvider } from '@auth0/nextjs-auth0/dist/frontend'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
